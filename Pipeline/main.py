7dbd8b0501bfd82c30f981f04f949deae0f1298f
Sahil Gupta <guptasahil926@gmail.com>
2020-09-07 13:50:08 -0700

    Added mne tools
    The program now eliminates the last column since that's only tells you the date and that was causing problems when trying to read the data. The file is then converted into RawArray object and is then plotted. The plot looks terrible right now so that still needs work.

--- Pipeline/main.py
+++ Pipeline/main.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 import pandas as pd
 import os
-
+import mne
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels, BoardIds
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
 
@@ -25,11 +25,38 @@ def main ():
             restored_df = pd.DataFrame (np.transpose (restored_data))
             print ('Data From the File')
             print (restored_df.head (10))
+        else:
+            break
 
     restored_data = DataFilter.read_file ('OpenBCI-RAW-2020-08-18_08-44-41.csv')
-    restored_df = pd.DataFrame (np.transpose (restored_data))
-    print ('Data From the File')
-    print (restored_df.head (10))
+    if (restored_data.shape[0] > 23):  # If the timestamp has not already been removed then we will remove it
+        new_data = np.delete(restored_data, 23,
+                             0)  # There to delete the date since that could not be converted into float
+        restored_df = pd.DataFrame(np.transpose(new_data))
+        DataFilter.write_file(new_data, "OpenBCI-RAW-2020-08-18_08-44-41.csv", 'w')
+    else:
+        restored_df = pd.DataFrame(np.transpose(restored_data))
+    print('Data From the File')
+    print(restored_df.head(10))
+
+    data = np.loadtxt("OpenBCI-RAW-2020-08-18_08-44-41.csv", delimiter=',')  # remember to remove the first three lines
+    data = np.transpose(data)
+
+    ch_names = ['EXG Channel 0', 'EXG Channel 1', 'EXG Channel 2', 'EXG Channel 3', 'EXG Channel 4', 'EXG Channel 5',
+                'EXG Channel 6', 'EXG Channel 7', 'Accel Channel 0', 'Accel Channel 1', 'Accel Channel 2', 'Other',
+                'Other', 'Other', 'Other', 'Other', 'Other', 'Other', 'Analog Channel 0', 'Analog Channel 1',
+                'Analog Channel 2',
+                'Timestamp', 'Timestamp(Formatted)']
+
+    sfreq = 250
+    info = mne.create_info(ch_names, sfreq, ch_types='emg')
+
+    raw = mne.io.RawArray(data, info)
+    print(raw)
+    print(raw.info)
+
+    raw.plot()
+    print(type(raw))
 
 
 if __name__ == "__main__":
