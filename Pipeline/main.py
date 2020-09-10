17c91416ccadffe9bbd367dca46d9bae26e8e5d8
Sahil Gupta <guptasahil926@gmail.com>
2020-09-09 18:59:33 -0700

    Added normalization but it doesn't quite work yet

--- Pipeline/main.py
+++ Pipeline/main.py
@@ -11,8 +11,7 @@ from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
 def main ():
 
 
-    # demo for data serialization using Pipeline API, we recommend to use it instead pandas.to_csv()
-    #DataFilter.write_file (data, 'test.csv', 'w') # use 'a' for append mode
+    #Convert text files to csv file. May possibly remove this part later. It's useless right now.
     directory = os.path.dirname(os.path.abspath(__file__))
 
     # renames .txt files to .csv and then prints its contents
@@ -28,6 +27,7 @@ def main ():
         else:
             break
 
+    #Use BrainFlow methods to read file containing raw EMG data.
     restored_data = DataFilter.read_file ('OpenBCI-RAW-2020-08-18_08-44-41.csv')
     if (restored_data.shape[0] > 23):  # If the timestamp has not already been removed then we will remove it
         new_data = np.delete(restored_data, 23,0)  # There to delete the date since that could not be converted into float
@@ -38,7 +38,8 @@ def main ():
     print('Data From the File')
     print(restored_df.head(10))
 
-    data = np.loadtxt("OpenBCI-RAW-2020-08-18_08-44-41.csv", delimiter=',')  # remember to remove the first three lines
+
+    data = np.loadtxt("OpenBCI-RAW-2020-08-18_08-44-41.csv", delimiter=',')  # remember to remove the first five lines
     data = np.transpose(data)
 
 
@@ -55,26 +56,39 @@ def main ():
     print(raw)
     print(raw.info)
 
-    
-    raw.plot(block = True, scalings=dict(mag=1e-12, grad=4e-11, eeg=20e-6, eog=150e-6, ecg=5e-4,
-     emg=1e2, ref_meg=1e-12, misc=1e-3, stim=1,
-     resp=1, chpi=1e-4, whitened=1e2))
-    print(type(raw))
+    # raw.plot(block = True, scalings=dict(mag=1e-12, grad=4e-11, eeg=20e-6, eog=150e-6, ecg=5e-4,
+    #  emg=1e2, ref_meg=1e-12, misc=1e-3, stim=1,
+    #  resp=1, chpi=1e-4, whitened=1e2))
 
     sfreq = 1000
     f_p = 40
 
+
+    #Applying butterworth filter
     iirs_params = dict(order = 4, ftype = 'butter', output = 'sos')
     iir_params = mne.filter.construct_iir_filter(iirs_params, f_p, None, sfreq, 'lowpass', return_copy = False, verbose = True)
     filtered_data = mne.filter.filter_data(data, sfreq = sfreq,l_freq = None, h_freq = f_p, picks = None, method = 'iir', iir_params = iir_params, copy = False, verbose = True)
 
     filtered_raw = mne.io.RawArray(filtered_data, info)
-    print(filtered_raw)
     print(filtered_raw.info)
-    filtered_raw.plot(block = True, scalings=dict(mag=1e-12, grad=4e-11, eeg=20e-6, eog=150e-6, ecg=5e-4,
-     emg=1e2, ref_meg=1e-12, misc=1e-3, stim=1,
-     resp=1, chpi=1e-4, whitened=1e2))
+    #filtered_raw.plot(block = True, scalings=dict(mag=1e-12, grad=4e-11, eeg=20e-6, eog=150e-6, ecg=5e-4,
+     #emg=1e2, ref_meg=1e-12, misc=1e-3, stim=1,
+     #resp=1, chpi=1e-4, whitened=1e2))
     print(type(filtered_raw))
+
+    filtered_raw_numpy = filtered_raw[:][0]
+    print(filtered_raw_numpy[0])
+    print(np.mean(filtered_raw_numpy, axis=0)[0]/np.std(filtered_raw_numpy, axis=0)[0])
+    normalized_raw = (filtered_raw_numpy) - (np.mean(filtered_raw_numpy, axis=0) / np.std(filtered_raw_numpy, axis=0))
+    normalized_raw = mne.io.RawArray(normalized_raw, info)
+    #normalized_raw.plot(block = True, scalings=dict(mag=1e-12, grad=4e-11, eeg=20e-6, eog=150e-6, ecg=5e-4,
+     #emg=1e2, ref_meg=1e-12, misc=1e-3, stim=1,
+     #resp=1, chpi=1e-4, whitened=1e2))
+
+
+
+# def normalize(dataset):
+#     return (filtered_raw - np.mean(filtered_raw, axis=0)) / np.std(filtered_raw, axis=0)
 
 
 
