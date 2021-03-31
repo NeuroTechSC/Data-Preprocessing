725f73d2f9eafa1fda8a6e531c1491adc9f4a6f8
luucyzheeng <lzheng20@ucsc.edu>
2021-03-30 18:12:13 -0700

    committing initial comparison code

--- splice.py
+++ splice.py
@@ -142,6 +142,19 @@ def web_parser():
 
 def compare(input_IPA, word_diction):
 
+	articulation = {'ŋ':0,'k':0,'g':0,'x':0,'w':0,'h':0,'tʃ':1,'dʒ':1,'tʃ':1,'dʒ':1,'ʃ':1,'ʒ':1,'r':1,'j':1,'n':2,'t':2,'d':2,'s':2,'z':2,'l':2,'θ':3,'ð':3,'m':4,'p':4,'b':4,'f':4,'v':4}
+	manor = {'ŋ':2,'k':0,'g':0,'x':0,'w':-1,'h':0,'tʃ':1,'dʒ':1,'tʃ':1,'dʒ':0,'ʃ':1,'ʒ':0,'r':-1,'j':-1,'n':2,'t':1,'d':0,'s':1,'z':0,'l':-1,'θ':1,'ð':0,'m':2,'p':1,'b':0,'f':1,'v':0}
+	occlusion = {'ŋ': -1, 'k': 0, 'g': 0, 'x': 1, 'w': 2, 'h': 1, 'tʃ': 0, 'dʒ': 0,
+				 'ʃ': 1, 'ʒ': 1, 'r': 2, 'j': 2, 'n': -1, 't': 0, 'd': 0, 's': 1, 'z': 1, 'l': 2, 'θ': 1,'ð':1,'m':-1,'p':0,'b':0,'f':1,'v':1}
+	diction_vectors = {}
+	for key in word_diction:
+		diction_vectors.append(key)
+		word_vectors = np.zeros((len(key),3))
+		for letters_index in range(key):
+			word_vectors[letters_index][0] = articulation[key[letters_index]]
+			word_vectors[letters_index][1] = manor[key[letters_index]]
+			word_vectors[letters_index][2] = occlusion[key[letters_index]]
+	print(diction_vectors)
 	best = get_close_matches(input_IPA, word_diction.values())
 	print(best)
 	# score = 0
